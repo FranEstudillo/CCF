f4c2f2be5378d69843ec94b633f029b617242c16
Francisco Estudillo <franciscoestudillo97@outlook.com>
2024-10-11 18:01:47 -0600

    Avances server

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const morgan = require("morgan");
+const myConn = require("express-myconnection");
+
 const app = express();
 
 // Settings
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
-// app.set("views", path.join(__dirname, "../views"));
-console.log(__dirname);
+app.set(
+  "views",
+  "C:/Users/valsistes01/Desktop/Fran/PROYECTOS/gestor-ccf-js/src/views"
+);
+
+//Middlewares
+app.use(morgan("dev"));
+
+//MySQL
+
 app.listen(app.get("port"), () => {
   console.log("Server on port 3000");
-  console.log(__dirname + "../");
 });
